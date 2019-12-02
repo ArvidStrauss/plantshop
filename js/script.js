89528c01ccd6db396ec756f18d74b55fbd52d737
Arvid Strauss <avst@mms-t-systems.de>
2019-12-02 12:29:51 +0100

    added product-similar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ fillShoppingCart(products);
 cartContent.addEventListener("click", removeCourse);
 function removeCourse(e) {
   if (e.target.classList.contains("remove")) {
+    foundIndex = products.findIndex(
+      x => x.id == e.target.getAttribute("data-id")
+    );
+    products.splice(foundIndex, 1);
+    window.localStorage.setItem("products", JSON.stringify(products));
+
     e.target.parentElement.parentElement.remove();
 
     wt.product = document.querySelector("h2#title-product").textContent;
@@ -17,17 +23,37 @@ function removeCourse(e) {
   }
 }
 if (document.querySelector("#add-to-cart")) {
+  let foundIndex;
+  let product = null;
   document.querySelector("#add-to-cart").addEventListener("click", () => {
-    const product = {
-      image: document.querySelector("img.img-one").src,
-      title: document.querySelector("h2#title-product").textContent,
-      price: document.querySelector(".price").textContent,
-      qty: document.querySelector("input#productQuantity").value,
-      id: document.querySelector("button#add-to-cart").getAttribute("data-id")
-    };
-    products.push(product);
+    if (
+      (foundIndex = products.findIndex(
+        product =>
+          product.id ==
+          document.querySelector("button#add-to-cart").getAttribute("data-id")
+      )) !== -1
+    ) {
+      products[foundIndex].qty += parseInt(
+        document.querySelector("input#productQuantity").value
+      );
+      document.querySelector(
+        `.product-${products[foundIndex].id}`
+      ).textContent = products[foundIndex].qty;
+    } else {
+      product = {
+        image: document.querySelector("img.img-one").src,
+        title: document.querySelector("h2#title-product").textContent,
+        price: document.querySelector(".price").textContent,
+        qty: parseInt(document.querySelector("input#productQuantity").value),
+        id: document.querySelector("button#add-to-cart").getAttribute("data-id")
+      };
+      products.push(product);
+
+      addItemToCart(product);
+    }
+
     window.localStorage.setItem("products", JSON.stringify(products));
-    addItemToCart(product);
+
     // send add Product to Webtrekk
     wt.product = document.querySelector("h2#title-product").textContent;
     wt.productQuantity = document.querySelector("input#productQuantity").value;
@@ -59,7 +85,7 @@ function addItemToCart(product) {
       <td>
         ${product.price}
       </td>
-      <td>
+      <td class="product-${product.id}">
         ${product.qty}
       </td>
       <td>
